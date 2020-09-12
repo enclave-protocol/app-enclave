7fb0660512628d02136e256cba194ff90904d0af
Enclave <>
2020-09-12 19:36:47 +0100

    Layout styles, scss plugin

--- components/Layout.js
+++ components/Layout.js
@@ -18,6 +18,62 @@ export default function Layout({children}) {
         </main>
         <Footer />
       </div>
+
+      <style jsx global>{`
+          html {
+            background-color: #282C31;
+            font-size: 24px;
+          }
+
+          body {
+            color: #fff;
+            overflow-y: scroll;
+            font-size: 24px;
+
+            &::-webkit-scrollbar {
+              width: 5px;
+            }
+
+            &::-webkit-scrollbar-track {
+              background: #000;
+            }
+
+            &::-webkit-scrollbar-thumb {
+              background-color: #000;
+            }
+          }
+
+          ::-moz-selection {
+            background: #3297FD;
+            color: #fff;
+            text-shadow: none;
+          }
+
+          ::selection {
+            background: #3297FD;
+            color: #fff;
+            text-shadow: none;
+          }
+        `}</style>
+
+        <style jsx>{`
+          div {
+            max-width: 1430px;
+            width: 100%;
+            margin: 0 auto;
+            padding: .5rem 1rem;
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+
+          main {
+            max-width: 787px;
+            width: 100%;
+            flex: 1 0 auto;
+            margin: 3rem auto 2.5rem;
+          }
+        `}</style>
     </>
   )
 }
