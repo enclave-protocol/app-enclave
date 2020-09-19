3946a93123025a338245092c27130dedbbbb0963
Enclave <>
2020-09-19 23:05:03 +0100

    Button

--- components/Button.js
+++ components/Button.js
@@ -1,10 +1,51 @@
-export default function Button({children, onClick}) {
+export default function Button({children, onClick, ...props}) {
 
   return (
       <>
-        <button onClick={onClick}>
+        <button onClick={onClick} {...props}>
           {children}
         </button>
+
+        <style jsx>{`
+          button {
+            position: relative;
+            background: #000;
+            box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.42);
+            border-radius: 19px;
+            padding: .3rem 0;
+            border: none;
+            outline: none;
+            font-size: 24px;
+            transition: color .2s linear, background .2s linear;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            cursor: pointer;
+
+            color: #fff;
+            text-shadow: #000;
+
+            &:hover {
+              color: #F48432;
+            }
+
+            &.active {
+              color: #FFFFFF;
+              background: #F48432;
+              box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.42);
+              text-shadow: none;
+
+              &:hover {
+                color: gray;
+              }
+            }
+
+            img {
+              height: 24px;
+              width: 24px;
+            }
+          }
+        `}</style>
       </>
   )
 }
