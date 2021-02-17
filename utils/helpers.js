51cca47d3f4f774ec33798b02eef47d53be871df
Enclave <>
2021-02-17 05:23:10 +0100

    Added user input automatic formatting

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,13 +1,17 @@
 export function normalizeBalance(balance) {
   if (balance.length > 18) {
-    const eth = balance.length - 18
-    if (eth < 5) {
-      return balance.substring(0, eth) + '.' + balance.substring(eth, 5 - eth)
+    const enzk = balance.length - 18
+    if (enzk < 5) {
+      return balance.substring(0, enzk) + '.' + balance.substring(enzk, 5 - enzk)
     }
     return balance.substring(0, 5)
   } else {
     return '0.' + balance.substring(0, 4)
   }
+}
+
+export function formatValue(value) {
+  return value.replace(',', '.').slice(0, 8)
 }
 
 export function normalizeAddress(addr) {
